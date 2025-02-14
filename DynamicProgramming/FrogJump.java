8d77b3563aebdb5145baacfd4c590e9b234ae7ae
Parth Sharma <parthsharma8421@gmail.com>
2025-02-15 04:57:06 +0530

    update

--- DynamicProgramming/FrogJump.java
+++ DynamicProgramming/FrogJump.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Solution {
     private int topDownHelper(int n, int[] height, int[] memo) {
@@ -12,10 +13,10 @@ class Solution {
         
         int oneStep = Integer.MAX_VALUE, twoStep = Integer.MAX_VALUE;
         if(n - 1 >= 0) {
-            oneStep = Math.abs(height[n] - height[n - 1]) + topDownHelper(n - 1, height);
+            oneStep = Math.abs(height[n] - height[n - 1]) + topDownHelper(n - 1, height, memo);
         }
         if(n - 2 >= 0) {
-            twoStep = Math.abs(height[n] - height[n - 2]) + topDownHelper(n - 2, height);
+            twoStep = Math.abs(height[n] - height[n - 2]) + topDownHelper(n - 2, height, memo);
         }
         memo[n] = Math.min(oneStep, twoStep);
         return memo[n];
@@ -24,7 +25,28 @@ class Solution {
     private int topDown(int[] height) {
         int n = height.length;
         int[] memo = new int[n];
+        Arrays.fill(memo, -1);
         return topDownHelper(n - 1, height, memo);
+    }
+
+    private int bottomUp(int[] height) {
+        int n = height.length;
+        int[] dp = new int[n];
+
+        dp[0] = 0;
+
+        for(int i=1; i<n; i++) {
+            int oneStep = Integer.MAX_VALUE, twoStep = Integer.MAX_VALUE;
+            if(i - 1 >= 0) {
+                oneStep = Math.abs(height[i] - height[i - 1]) + dp[i - 1];
+            }
+            if(i - 2 >= 0) {
+                twoStep = Math.abs(height[i] - height[i - 2]) + dp[i - 2];
+            }
+            dp[i] = Math.min(oneStep, twoStep);
+        }
+
+        return dp[n - 1];
     }
 }
 
