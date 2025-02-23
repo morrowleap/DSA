f1e82e5dd75baea2d67e3eefa4f21e195f5a70ad
Parth Sharma <parthsharma8421@gmail.com>
2025-02-23 14:26:48 +0530

    update

--- DynamicProgramming/MinimalCost.java
+++ DynamicProgramming/MinimalCost.java
@@ -1,51 +1,23 @@
 import java.util.Scanner;
 
 class Solution {
-    private int topDownHelper(int n, int k, int[] height, int[] memo) {
-        if(n <= 0) {
+    private int topDownHelper(int n, int k, int[] arr) {
+        if(n == 0) {
             return 0;
         }
-
-        if(memo[n] != -1) {
-            return memo[n];
-        }
-
         int minCost = Integer.MAX_VALUE;
         for(int jump = 1; jump <= k; jump++) {
             if(n - jump >= 0) {
-                int stepCost = Math.abs(height[n] - height[n - jump]) + topDownHelper(n - jump, k, height, memo);
-                minCost = Math.min(minCost, stepCost);
+                int cost = Math.abs(arr[n] - arr[n - jump]) + topDownHelper(n - jump, k, arr);
+                minCost = Math.min(minCost, cost);
             }
         }
-
-        memo[n] = minCost;
         return minCost;
     }
 
-    private int topDown(int k, int[] height) {
-        int n = height.length;
-        int[] memo = new int[n];
-        for(int i = 0; i < n; i++) {memo[i] = -1;}
-        return topDownHelper(n - 1, k, height, memo);
-    }
-
-    private int bottomUp(int k, int[] height) {
-        int n = height.length;
-        int[] dp = new int[n];
-
-        dp[0] = 0;
-        for(int i = 1; i < n; i++) {
-            int minCost = Integer.MAX_VALUE;
-            for(int jump = 1; jump <= k; jump++) {
-                if(i - jump >= 0) {
-                    int stepCost = Math.abs(height[i] - height[i - jump]) + dp[i - jump];
-                    minCost = Math.min(minCost, stepCost);
-                }
-            }
-            dp[i] = minCost;
-        }
-
-        return dp[n - 1];
+    public int topDown(int k, int[] arr) {
+        int n = arr.length;
+        return topDownHelper(n - 1, k, arr);
     }
 }
 
@@ -53,12 +25,15 @@ public class MinimalCost {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] height = {10, 30, 40, 50, 20};
-        int k = 3;
+        int k = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] heights = new int[n];
+        for(int i=0;i<n;i++) {
+            heights[i] = scanner.nextInt();
+        }
 
         Solution sol = new Solution();
-        System.out.println(sol.topDown(k, height));
-        System.out.println(sol.bottomUp(k, height));
+        System.out.println(sol.topDown(k, heights));
 
         scanner.close();
     }
