fbe4048f5e7c4bcb5d889cc33db9eb54d64d7aa2
Parth Sharma <parthsharma8421@gmail.com>
2025-02-26 02:51:03 +0530

    update

--- DynamicProgramming/UniquePaths2.java
+++ DynamicProgramming/UniquePaths2.java
@@ -37,6 +37,44 @@ class Solution {
 
         return topDownHelper(m - 1, n - 1, grid, memo);
     }
+
+    public int bottomUp(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] memo = new int[m][n];
+
+        if(grid[0][0] == 1) {
+            return 0;
+        }
+
+        memo[0][0] = 1;
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+
+                if(i == 0 && j == 0) {
+                    continue;
+                }
+
+                if(grid[i][j] == 1) {
+                    memo[i][j] = 0;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if(i - 1 >= 0 && grid[i - 1][j] != 1) {
+                    up = memo[i - 1][j];
+                }
+                if(j - 1 >= 0 && grid[i][j - 1] != 1) {
+                    left = memo[i][j - 1];
+                }
+                
+                memo[i][j] = up + left;
+            }
+        }
+        return memo[m - 1][n - 1];
+    }
 }
 
 public class UniquePaths2 {
