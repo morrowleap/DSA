5678e3d6106f04fc57fe0bbb3a6ff11b0df82376
Parth Sharma <parthsharma8421@gmail.com>
2025-03-04 22:33:59 +0530

    update

--- DynamicProgramming/MinimumFallingPathSum.java
+++ DynamicProgramming/MinimumFallingPathSum.java
@@ -3,32 +3,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    private int topDownHelper(int[][] matrix, int row, int col) {
+    private int topDownHelper(int[][] matrix, int row, int col, int[][] memo) {
         int n = matrix.length;
 
         if(row == n - 1) {
             return matrix[row][col];
         }
+
+        if(memo[row][col] != -1) {
+            return memo[row][col];
+        }
         
         int mini = Integer.MAX_VALUE;
         if(row + 1 < n && col - 1 >= 0) {
-            mini = Math.min(mini, topDownHelper(matrix, row + 1, col - 1));
+            mini = Math.min(mini, topDownHelper(matrix, row + 1, col - 1, memo));
         }
         if(row + 1 < n && col < n) {
-            mini = Math.min(mini, topDownHelper(matrix, row + 1, col));
+            mini = Math.min(mini, topDownHelper(matrix, row + 1, col, memo));
         }
         if(row + 1 < n && col + 1 < n) {
-            mini = Math.min(mini, topDownHelper(matrix, row + 1, col + 1));
+            mini = Math.min(mini, topDownHelper(matrix, row + 1, col + 1, memo));
         }
 
-        return matrix[row][col] + mini;
+        memo[row][col] = matrix[row][col] + mini;
+        return memo[row][col];
     }
 
     public int topDown(int[][] matrix) {
         int n = matrix.length;
+        int[][] memo = new int[n][n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                memo[i][j] = -1;
+            }
+        }
+
         int mini = Integer.MAX_VALUE;
         for(int i=0;i<n;i++) {
-            mini = Math.min(mini, topDownHelper(matrix, 0, i));
+            mini = Math.min(mini, topDownHelper(matrix, 0, i, memo));
         }
         return mini;
     }
