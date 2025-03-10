ddb6ebed1d3bbb1c8d0950c871e68b95ead278a6
Parth Sharma <parthsharma8421@gmail.com>
2025-03-10 19:10:04 +0530

    update

--- dynamic_programming/SubsetSum.java
+++ dynamic_programming/SubsetSum.java
@@ -14,19 +14,50 @@ class SubsetSumSolution {
             return true;
         }
 
-        if (n < 0 || target < 0) {
-            return false;
+        if (n == 0) {
+            return nums[0] == target;
         }
 
         if (memo[n][target] != null) {
             return memo[n][target];
         }
 
-        boolean pick = topDownHelper(nums, n - 1, target - nums[n], memo);
+        boolean pick = false;
+        if(target - nums[n] >= 0) {
+            pick = topDownHelper(nums, n - 1, target - nums[n], memo);
+        }
         boolean notPick = topDownHelper(nums, n - 1, target, memo);
 
         memo[n][target] = pick || notPick;
         return memo[n][target];
+    }
+
+    public Boolean bottomUp(int[] nums, int target) {
+        // Base Case 1: if at any point target is 1 return true
+        int n = nums.length;
+        boolean[][] dp = new boolean[n][target + 1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                boolean pick = false;
+                if(j - nums[i] >= 0) {
+                    pick = dp[i - 1][j - nums[i]];
+                }
+                boolean notPick = dp[i - 1][j];
+
+                dp[i][j] = pick || notPick;
+            }
+        }
+
+        return dp[n - 1][target];
     }
 }
 
@@ -44,6 +75,7 @@ public class SubsetSum {
 
         SubsetSumSolution sol = new SubsetSumSolution();
         System.out.println(sol.topDown(nums, target));
+        System.out.println(sol.bottomUp(nums, target));
 
         sc.close();
     }
