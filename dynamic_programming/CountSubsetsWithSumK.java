dc7688b6b3ad3eaf363d46ea8f5b40cb63073135
Parth Sharma <parthsharma8421@gmail.com>
2025-03-15 01:48:14 +0530

    update

--- dynamic_programming/CountSubsetsWithSumK.java
+++ dynamic_programming/CountSubsetsWithSumK.java
@@ -3,26 +3,31 @@ package dynamic_programming;
 import java.util.Scanner;
 
 class CountSubsetsWithSumKSolution {
-
     public int topDown(int[] nums, int target) {
         int n = nums.length;
-        return topDownHelper(nums, n - 1, target);
+        Integer[][] memo = new Integer[n][target + 1];
+        return topDownHelper(nums, n - 1, target, memo);
     }
 
-    private int topDownHelper(int[] nums, int n, int target) {
+    private int topDownHelper(int[] nums, int n, int target, Integer[][] memo) {
         if (n == 0) {
             if(target == 0 && nums[0] == 0) {return 2;}
             if(target == 0 || nums[0] == target) {return 1;}
             return 0;
         }
 
-        int notPick = topDownHelper(nums, n - 1, target);
-        int pick = 0;
-        if (nums[n] <= target) {
-            pick = topDownHelper(nums, n - 1, target - nums[n]);
+        if(memo[n][target] != null) {
+            return memo[n][target];
         }
 
-        return pick + notPick;
+        int notPick = topDownHelper(nums, n - 1, target, memo);
+        int pick = 0;
+        if (nums[n] <= target) {
+            pick = topDownHelper(nums, n - 1, target - nums[n], memo);
+        }
+
+        memo[n][target] = pick + notPick;
+        return memo[n][target];
     }
 }
 
