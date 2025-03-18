ddc35e7e9f51d0914a8df6a3c32dcf554b82a191
Parth Sharma <parthsharma8421@gmail.com>
2025-03-18 16:32:06 +0530

    update

--- dynamic_programming/TargetSum.java
+++ dynamic_programming/TargetSum.java
@@ -5,18 +5,22 @@
 
 package dynamic_programming;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class TargetSumSolution1 {
 
     public int topDown(int[] nums, int target) {
         int n = nums.length;
-        return topDownHelper(nums, n - 1, target);
+        int sum = Arrays.stream(nums).sum();
+        int offset = Math.abs(sum);
+        Integer[][] memo = new Integer[n][2 * offset + 1];
+        return topDownHelper(nums, n - 1, 0, target, memo, offset);
     }
 
-    private int topDownHelper(int[] nums, int n, int target) {
+    private int topDownHelper(int[] nums, int n, int sum, int target, Integer[][] memo, int offset) {
         if (n == 0) {
-            if (nums[0] == Math.abs(target)) {
+            if (nums[0] == Math.abs(target - sum)) {
                 if (nums[0] == 0) {
                     return 2;
                 } else {
@@ -27,10 +31,14 @@ class TargetSumSolution1 {
             }
         }
         
-        int pos = topDownHelper(nums, n - 1, target + nums[n]);
-        int neg = topDownHelper(nums, n - 1, target - nums[n]);
+        if (memo[n][sum + offset] != null) 
+            return memo[n][sum + offset];
+        
+        int pos = topDownHelper(nums, n - 1, sum + nums[n], target, memo, offset);
+        int neg = topDownHelper(nums, n - 1, sum - nums[n], target, memo, offset);
 
-        return pos + neg;
+        memo[n][sum + offset] = pos + neg;
+        return memo[n][sum + offset];
     }
 }
 
