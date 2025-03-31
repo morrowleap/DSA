77a6b6e392460e6fcf88c251d1e5633d046bdec7
Parth Sharma <parthsharma8421@gmail.com>
2025-04-01 03:06:12 +0530

    update

--- dynamic_programming/LongestCommonSubsequence.java
+++ dynamic_programming/LongestCommonSubsequence.java
@@ -37,6 +37,40 @@ class LongestCommonSubsequenceSol {
         return memo[m][n];
     }
 
+    public int bottomUp(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        // Shifting of indexes, for writing the tabulation
+        // the indexs are shifted by 1
+        // In topdown recursion started from m-1,n-1 to -1,-1
+        // here it will start from m,n to 0,0
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    continue;
+                }
+
+                int check1 = dp[i - 1][j];
+                int check2 = dp[i][j - 1];
+
+                dp[i][j] = Math.max(check1, check2);
+            }
+        }
+
+        return dp[m][n];
+    }
+
 }
 
 public class LongestCommonSubsequence {
