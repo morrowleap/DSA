b787e823440dbf09f7187f5a4caa315ac428c5c5
Parth Sharma <parthsharma8421@gmail.com>
2025-04-07 19:28:11 +0530

    update

--- dynamic_programming/LongestCommonSubstring.java
+++ dynamic_programming/LongestCommonSubstring.java
@@ -15,8 +15,32 @@ class LongestCommonSubstringSol {
     }
 
     private int bottomUp(String text1, String text2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'bottomUp'");
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        int maxi = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    maxi = Math.max(maxi, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return maxi;
     }
 
     private void topDown(String text1, int i, String text2, int j, int[][] memo) {
