4dfd229b7ff1d7141e01fdada71183e3ef7b0bba
Parth Sharma <parthsharma8421@gmail.com>
2025-04-13 11:19:15 +0530

    update

--- recursion/LetterCombinationsOfAPhoneNumber.java
+++ recursion/LetterCombinationsOfAPhoneNumber.java
@@ -4,13 +4,49 @@
 
 package recursion;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LetterCombinationsOfAPhoneNumber {
 
     public List<String> letterCombinations(String digits) {
-        return null;
+        Map<Character, String> keyMapping = new HashMap<Character, String>() {
+            {
+                put('2', "abc");
+                put('3', "def");
+                put('4', "ghi");
+                put('5', "jkl");
+                put('6', "mno");
+                put('7', "pqrs");
+                put('8', "tuv");
+                put('9', "wxyz");
+            }
+        };
+
+        StringBuilder curr = new StringBuilder();
+        List<String> res = new ArrayList<>();
+        topDown(keyMapping, digits, 0, curr, res);
+
+        return res;
+    }
+
+    private void topDown(Map<Character, String> keyMapping, String digits, int i, StringBuilder curr,
+            List<String> res) {
+        if (i == digits.length()) {
+            if (!curr.isEmpty()) {
+                res.add(curr.toString());
+            }
+            return;
+        }
+
+        for (char x : keyMapping.get(digits.charAt(i)).toCharArray()) {
+            curr.append(x);
+            topDown(keyMapping, digits, i + 1, curr, res);
+            curr.deleteCharAt(curr.length() - 1);
+        }
     }
 
     public static void main(String[] args) {
