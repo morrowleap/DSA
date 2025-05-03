0b0a8467183e97ceb1302874b18567b3d02f33e6
Parth Sharma <parthsharma8421@gmail.com>
2025-05-03 20:05:42 +0530

    update

--- arrays/MajorityElementMoreeVotingAlgo.java
+++ arrays/MajorityElementMoreeVotingAlgo.java
@@ -8,7 +8,6 @@
 package arrays;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MajorityElementMoreeVotingAlgo {
@@ -16,23 +15,62 @@ public class MajorityElementMoreeVotingAlgo {
         int nums[] = { 3, 2, 3 };
 
         MajorityElementMoreeVotingAlgo sol = new MajorityElementMoreeVotingAlgo();
-        System.out.println(sol.majorityElement1(nums));
+        System.out.println(sol.majorityElement2(nums));
     }
 
     /**
-     * Better Approach: Hashing
+     * Moore Voting Algo
+     * T.C: O(N)
+     * S.C: O(1)
+     */
+    public int majorityElement2(int[] nums) {
+        int count = 0;
+        int ele = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                ele = num;
+            }
+
+            if (ele == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify if moree algo produced right candidate
+        count = 0;
+        for (int num : nums) {
+            if (num == ele) {
+                count++;
+            }
+        }
+        if (count > nums.length / 2) {
+            return ele;
+        }
+        return -1;
+    }
+
+    /**
+     * Hashing Approach
      * T.C: O(N)
      * S.C: O(N)
      */
-    public List<Integer> majorityElement1(int[] nums) {
+    public int majorityElement1(int[] nums) {
         int n = nums.length;
         HashMap<Integer, Integer> hash = new HashMap<>();
         for (int num : nums) {
             hash.put(num, hash.getOrDefault(num, 0) + 1);
         }
         for (Map.Entry<Integer, Integer> e : hash.entrySet()) {
+            int num = e.getKey();
+            int count = e.getValue();
 
+            if (count > n / 2) {
+                return num;
+            }
         }
-        return null;
+        return -1;
     }
 }
