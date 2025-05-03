f08b63c90cc597cd8c1c2993e0853a7924d01278
Parth Sharma <parthsharma8421@gmail.com>
2025-05-03 11:02:09 +0530

    update

--- recursion/CombinationSumII.java
+++ recursion/CombinationSumII.java
@@ -13,80 +13,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CombinationSumII {
-
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-
-        Arrays.sort(candidates);
-
-        topDown(candidates, 0, target, curr, res);
-        topDown2(candidates, 0, target, curr, res);
-
-        return res;
-    }
-
-    private void topDown(int[] nums, int i, int target, List<Integer> curr, List<List<Integer>> res) {
-        if (i == nums.length || target < 0) {
-            if (target == 0) {
-                res.add(new ArrayList<>(curr));
-            }
-            return;
-        }
-
-        curr.add(nums[i]);
-        topDown(nums, i + 1, target - nums[i], curr, res);
-        curr.remove(curr.size() - 1);
-
-        // After choosing above one, element is filled at 0th index of this subset, now
-        // in case of
-        // non pick it will go further in recursion, where in next recursion it will
-        // pick element for 0th index of another subset, so if the next element which we
-        // are shifting to
-        // for choices of picking it or not picking it, if that element is equal to our
-        // current element which we have already used in a subset at 0th index, we will
-        // ignore that for another subsets at 0th index
-        while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-            i++; // skipping duplicate elements
-        }
-        topDown(nums, i + 1, target, curr, res);
-
-        // Why index is not incremented in the "pick" part ? because it would miss valid
-        // combinations because the next recursive depth would skip the immediate next
-        // candidate unnecessarily.
-
-        // TODO: Attach recursion tree
-    }
-
-    private void topDown2(int[] nums, int index, int target, List<Integer> curr, List<List<Integer>> res) {
-        if (target == 0) {
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-
-        if (index == nums.length || target < 0) {
-            return;
-        }
-
-        for (int i = index; i < nums.length; i++) {
-            curr.add(nums[i]);
-            topDown2(nums, i + 1, target - nums[i], curr, res);
-            curr.remove(curr.size() - 1);
-
-            // In this approach we are generating subsets for a specific index, lets say we
-            // put value 1 at 0th index in a subset, now in other subset doing the same
-            // would not be good so with the below while loop we skip the duplicate element
-            // at
-            // same index for any other subset
-            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                i++; // skipping duplicate elements
-            } // This loop shifts pinter to last similar element, then for loop shifts pointer
-              // to next element
-        }
-
-        // TODO: Attach recursion tree
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -104,6 +30,88 @@ public class CombinationSumII {
         sc.close();
 
     }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        Arrays.sort(candidates);
+
+        // backtrack(candidates, 0, target, curr, res);
+        backtrack2(candidates, 0, target, curr, res);
+
+        return res;
+    }
+
+    public void backtrack2(int[] nums, int index, int target, List<Integer> curr, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if (index == nums.length || target < 0) {
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);
+            backtrack2(nums, i + 1, target - nums[i], curr, res);
+            curr.remove(curr.size() - 1);
+
+            // After choosing above one, element is filled at 0th index of this subset, now
+            // in case of
+            // non pick it will go further in recursion, where in next recursion it will
+            // pick element for 0th index of another subset, so if the next element which we
+            // are shifting to
+            // for choices of picking it or not picking it, if that element is equal to our
+            // current element which we have already used in a subset at 0th index, we will
+            // ignore that for another subsets at 0th index
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++; // skipping duplicate elements
+            }
+
+            // Why index is not incremented in the "pick" part ? because it would miss valid
+            // combinations because the next recursive depth would skip the immediate next
+            // candidate unnecessarily.
+
+            // TODO: Attach recursion tree
+        }
+    }
+
+    public void backtrack(int[] nums, int index, int target, List<Integer> curr, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if (target < 0 || index == nums.length) {
+            return;
+        }
+
+        curr.add(nums[index]);
+        backtrack(nums, index + 1, target - nums[index], curr, res);
+        curr.remove(curr.size() - 1);
+
+        // After choosing above one, element is filled at 0th index of this subset, now
+        // in case of
+        // non pick it will go further in recursion, where in next recursion it will
+        // pick element for 0th index of another subset, so if the next element which we
+        // are shifting to
+        // for choices of picking it or not picking it, if that element is equal to our
+        // current element which we have already used in a subset at 0th index, we will
+        // ignore that for another subsets at 0th index
+        while (index < nums.length - 1 && nums[index] == nums[index + 1]) {
+            index++; // skipping duplicate elements
+        }
+        backtrack(nums, index + 1, target, curr, res);
+
+        // Why index is not incremented in the "pick" part ? because it would miss valid
+        // combinations because the next recursive depth would skip the immediate next
+        // candidate unnecessarily.
+
+        // TODO: Attach recursion tree
+    }
+
 }
 
 // Recursive Time Complexity: O(2^N)
