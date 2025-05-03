e79e2bf9a21fc72a807693b29dd993d619e2a37f
Parth Sharma <parthsharma8421@gmail.com>
2025-05-03 20:36:40 +0530

    update

---
+++ arrays/MajorityElement2.java
@@ -0,0 +1,24 @@
+/*
+ * https://takeuforward.org/data-structure/majority-elementsn-3-times-find-the-elements-that-appears-more-than-n-3-times-in-the-array/
+ * https://youtu.be/vwZj1K0e9U8
+ * 
+ * https://leetcode.com/problems/majority-element-ii/description/
+*/
+
+package arrays;
+
+import java.util.List;
+
+public class MajorityElement2 {
+    public static void main(String[] args) {
+        int nums[] = { 2, 1, 1, 3, 1, 4, 5, 6 };
+
+        MajorityElement2 sol = new MajorityElement2();
+        System.out.println(sol.majorityElement(nums));
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        
+        return null;
+    }
+}
