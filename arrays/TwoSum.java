802563dbc8976f315443495bec98372eac98142a
Parth Sharma <parthsharma8421@gmail.com>
2025-05-03 19:43:35 +0530

    update

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -13,22 +13,20 @@ import java.util.Map;
 
 public class TwoSum {
 
-    /**
-     * Brute-Force Approach
-     */
-    public int[] twoSumBrute(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == target - nums[j]) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        return new int[] { -1, -1 };
+    public static void main(String[] args) {
+        int nums[] = { 2, 7, 11, 15 }, target = 9;
+
+        TwoSum sol = new TwoSum();
+        int res[] = sol.twoSum2(nums, target);
+        System.out.println(Arrays.toString(res));
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    /**
+     * Hashing Approach
+     * T.C: O(N)
+     * S.C: O(N)
+     */
+    public int[] twoSum2(int[] nums, int target) {
         int n = nums.length;
         Map<Integer, Integer> hash = new HashMap<>(); // Store curr, index
         for (int i = 0; i < n; i++) {
@@ -41,11 +39,21 @@ public class TwoSum {
         return new int[] { -1, -1 };
     }
 
-    public static void main(String[] args) {
-        int nums[] = { 2, 7, 11, 15 }, target = 9;
-
-        TwoSum sol = new TwoSum();
-        int res[] = sol.twoSum(nums, target);
-        System.out.println(Arrays.toString(res));
+    /**
+     * Brute-Force
+     * T.C: O(N^2)
+     * S.C: O(1)
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == target - nums[j]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
     }
+
 }
