06b6aab062ae87d1007680042c168df51a3e2ca3
Parth Sharma <parthsharma8421@gmail.com>
2025-05-03 11:32:29 +0530

    update

--- recursion/SubsetII.java
+++ recursion/SubsetII.java
@@ -1,7 +1,8 @@
 /*
  * https://takeuforward.org/data-structure/subset-ii-print-all-the-unique-subsets/
- * https://leetcode.com/problems/subsets-ii/description/
  * https://youtu.be/RIn3gOkbhQE
+ * 
+ * https://leetcode.com/problems/subsets-ii/description/
 */
 
 package recursion;
@@ -12,56 +13,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SubsetII {
-
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> curr = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        topDown(nums, 0, curr, res);
-        topDown2(nums, 0, curr, res);
-        return res;
-    }
-
-    /**
-     * For-Loop making subsequences technique
-     */
-    private void topDown2(int[] nums, int index, List<Integer> curr, List<List<Integer>> res) {
-        res.add(new ArrayList<>(curr));
-
-        for (int i = index; i < nums.length; i++) {
-            curr.add(nums[i]);
-            topDown2(nums, i + 1, curr, res);
-            curr.remove(curr.size() - 1);
-
-            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                i++;
-            }
-        }
-
-        // TODO: Attach recursion tree
-    }
-
-    /**
-     * Pick and non pick technique
-     */
-    private void topDown(int[] nums, int i, List<Integer> curr, List<List<Integer>> res) {
-        if (i == nums.length) {
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-
-        curr.add(nums[i]);
-        topDown(nums, i + 1, curr, res);
-        curr.remove(curr.size() - 1);
-
-        while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-            i++; // Skipping duplicates, same as Combination Sum 2
-        }
-        topDown(nums, i + 1, curr, res);
-
-        // TODO: Attach recursion tree
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -75,6 +26,57 @@ public class SubsetII {
         System.out.println(sol.subsetsWithDup(nums));
 
         sc.close();
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        Arrays.sort(nums);
+        // backtrack(nums, 0, curr, res);
+        backtrack2(nums, 0, curr, res);
+
+        return res;
+    }
+
+    /**
+     * For-Loop making subsequences technique
+     */
+    public void backtrack2(int[] nums, int index, List<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+
+        for (int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);
+            backtrack2(nums, i + 1, curr, res);
+            curr.remove(curr.size() - 1);
+
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+
+        // TODO: Attach recursion tree
+    }
+
+    /**
+     * Pick and non pick technique
+     */
+    public void backtrack(int[] nums, int index, List<Integer> curr, List<List<Integer>> res) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        curr.add(nums[index]);
+        backtrack(nums, index + 1, curr, res);
+        curr.remove(curr.size() - 1);
+
+        while (index < nums.length - 1 && nums[index] == nums[index + 1]) {
+            index++; // Skipping duplicates, same as Combination Sum 2
+        }
+        backtrack(nums, index + 1, curr, res);
+
+        // TODO: Attach recursion tree
     }
 }
 
