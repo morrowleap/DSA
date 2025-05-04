64c3da8e7a4d5c90fd7b8074741b1f49151488b0
Parth Sharma <parthsharma8421@gmail.com>
2025-05-04 11:37:14 +0530

    update

--- recursion/PalindromePartitioning.java
+++ recursion/PalindromePartitioning.java
@@ -1,7 +1,8 @@
 /*
  * https://takeuforward.org/data-structure/palindrome-partitioning/
- * https://leetcode.com/problems/palindrome-partitioning/description/
  * https://youtu.be/WBgsABoClE0
+ * 
+ * https://leetcode.com/problems/palindrome-partitioning/description/
 */
 
 package recursion;
@@ -11,43 +12,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PalindromePartitioning {
-
-    private List<List<String>> partition(String s) {
-        List<String> curr = new ArrayList<>();
-        List<List<String>> res = new ArrayList<>();
-        topDown(s, 0, curr, res);
-
-        return res;
-    }
-
-    private void topDown(String s, int index, List<String> curr, List<List<String>> res) {
-        if (index == s.length()) {
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-
-        for (int i = index; i < s.length(); i++) {
-            if (isPalindrome(s, index, i)) {
-                curr.add(s.substring(index, i + 1));
-                topDown(s, i + 1, curr, res);
-                curr.remove(curr.size() - 1);
-            }
-        }
-
-        // TODO: Attach recursion tree
-    }
-
-    private boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -57,6 +21,47 @@ public class PalindromePartitioning {
         System.out.println(sol.partition(s));
 
         sc.close();
+    }
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+
+        backtrack(s, 0, curr, res);
+
+        return res;
+    }
+
+    private void backtrack(String s, int index, List<String> curr, List<List<String>> res) {
+        if (index == s.length()) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+
+            String substr = s.substring(index, i + 1);
+
+            if (isPalindrome(substr)) {
+                curr.add(substr);
+                backtrack(s, i + 1, curr, res);
+                curr.remove(curr.size() - 1);
+            }
+        }
+
+        // TODO: Attach recursion tree
+    }
+
+    private boolean isPalindrome(String substr) {
+        int l = 0, r = substr.length() - 1;
+        while (l < r) {
+            if (substr.charAt(l) != substr.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
     }
 }
 
