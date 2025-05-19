4199e4544ba0110417b6187a47d2da73c5aa896b
Parth Sharma <z@Parth.local>
2025-05-20 03:37:42 +0530

    update

---
+++ linked_list/SortLL.java
@@ -0,0 +1,61 @@
+package linked_list;
+
+public class SortLL {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode head = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+        if (list1 != null) {
+            head.next = list1;
+        }
+        if (list2 != null) {
+            head.next = list2;
+        }
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return mergeTwoLists(left, right);
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-1);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(0);
+
+        SortLL r = new SortLL();
+        ListNode rev = r.sortList(head);
+
+        while (rev != null) {
+            System.out.print(rev.val + " ");
+            rev = rev.next;
+        }
+    }
+}
