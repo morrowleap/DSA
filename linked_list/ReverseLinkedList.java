9c91cf3747539ec07680168919167db79ca3066f
Parth Sharma <z@Parth.local>
2025-05-19 23:50:35 +0530

    update

---
+++ linked_list/ReverseLinkedList.java
@@ -0,0 +1,56 @@
+/*
+ * https://leetcode.com/problems/reverse-linked-list/
+ * 
+ * https://takeuforward.org/data-structure/reverse-a-linked-list/
+ * https://youtu.be/iRtLEoL-r-g
+ * 
+*/
+
+package linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ReverseLinkedList r = new ReverseLinkedList();
+        ListNode rev = r.reverseList(head);
+
+        while (rev != null) {
+            System.out.print(rev.val + " ");
+            rev = rev.next;
+        }
+    }
+}
---
+++ linked_list/ReverseLinkedList2.java
@@ -0,0 +1,66 @@
+/*
+ * https://leetcode.com/problems/reverse-linked-list-ii/
+*/
+
+package linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList2 {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode pl = dummy, l = head;
+
+        for (int i = 1; i < left; i++) {
+            pl = l;
+            l = l.next;
+        }
+
+        ListNode prev = null, curr = l, next = null;
+        for (int i = left; i <= right && curr != null; i++) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode r = curr;
+        pl.next.next = r;
+        pl.next = prev;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ReverseLinkedList2 r = new ReverseLinkedList2();
+        ListNode rev = r.reverseBetween(head, 2, 4);
+
+        while (rev != null) {
+            System.out.print(rev.val + " ");
+            rev = rev.next;
+        }
+    }
+}
