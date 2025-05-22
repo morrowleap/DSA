09334574aeedfd653f4f310762c5ada21d4aa497
Parth Sharma <z@Parth.local>
2025-05-22 21:33:07 +0530

    update

--- trees/BuildTreeFromInorderAndPostorder.java
+++ trees/BuildTreeFromInorderAndPostorder.java
@@ -1,19 +1,54 @@
 package trees;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BuildTreeFromInorderAndPostorder {
-    public static TreeNode buildTree(ArrayList<Integer> inOrder, ArrayList<Integer> postorder) {
-        return null;
+
+    private static ArrayDeque<Integer> postorderStack = new ArrayDeque<>();
+
+    public static TreeNode buildTree(ArrayList<Integer> inorder, ArrayList<Integer> postorder) {
+        for (int x : postorder) {
+            postorderStack.push(x);
+        }
+        TreeNode root = dfs(inorder);
+        return root;
+    }
+
+    private static TreeNode dfs(ArrayList<Integer> inorder) {
+        if (inorder.size() == 0) {
+            return null;
+        }
+
+        int val = postorderStack.pop();
+        TreeNode node = new TreeNode(val);
+
+        int index = 0;
+        while (index < inorder.size()) {
+            if (inorder.get(index) == val) {
+                break;
+            }
+            index++;
+        }
+        ArrayList<Integer> left = new ArrayList<>(inorder.subList(0, index));
+        ArrayList<Integer> right = new ArrayList<>(inorder.subList(index + 1, inorder.size()));
+
+        node.right = dfs(right);
+        node.left = dfs(left);
+
+        return node;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> inOrder = new ArrayList<>(Arrays.asList(6, 1, 3, 2));
-        ArrayList<Integer> postOrder = new ArrayList<>(Arrays.asList(6, 3, 2, 1));
+        ArrayList<Integer> inorder = new ArrayList<>(Arrays.asList(4, 2, 5, 1, 6, 3, 7));
+        ArrayList<Integer> postOrder = new ArrayList<>(Arrays.asList(4, 5, 2, 6, 7, 3, 1));
 
-        TreeNode root = buildTree(inOrder, postOrder);
+        TreeNode root = buildTree(inorder, postOrder);
 
-        
+        TreeNode.log(root);
+
+        System.out.println(TreeTraversal.inOrderTraversal(root));
+        System.out.println(TreeTraversal.postOrderTraversal(root));
     }
 }
--- trees/TreeTraversal.java
+++ trees/TreeTraversal.java
@@ -44,17 +44,17 @@ public class TreeTraversal {
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
-        // root.left = new TreeNode(2);
-        // root.right = new TreeNode(3);
-        // root.left.left = new TreeNode(4);
-        // root.left.right = new TreeNode(5);
-        // root.right.left = new TreeNode(6);
-        // root.right.right = new TreeNode(7);
-
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
         root.right.left = new TreeNode(6);
-        root.right.left.right = new TreeNode(7);
+        root.right.right = new TreeNode(7);
+
+        // root.left = new TreeNode(2);
+        // root.right = new TreeNode(3);
+        // root.right.left = new TreeNode(6);
+        // root.right.left.right = new TreeNode(7);
 
         TreeNode.log(root);
 
