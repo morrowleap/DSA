675842789f887fe2b787bc8b6570c00237b5abe3
Parth Sharma <z@Parth.local>
2025-05-25 21:20:33 +0530

    update

---
+++ trees/LowestCommonAncestorBinarySearchTree.java
@@ -0,0 +1,19 @@
+/*
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+*/
+
+package trees;
+
+public class LowestCommonAncestorBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
