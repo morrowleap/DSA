1d0cb581fb1ef1e6438f0f72af8be93f8e999e2f
Parth Sharma <z@Parth.local>
2025-05-26 03:58:33 +0530

    update

---
+++ trees/SortedArrayToBinarySearchTree.java
@@ -0,0 +1,21 @@
+/*
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+*/
+
+package trees;
+
+public class SortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -10, -3, 0, 5, 9 };
+
+        TreeNode root = new SortedArrayToBinarySearchTree().sortedArrayToBST(nums);
+
+        System.out.println(BuildTreeSerializeAndDeserializeBinaryTree.serialize(root));
+
+        TreeNode.log(root);
+    }
+}
