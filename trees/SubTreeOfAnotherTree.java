1187356107e2bed934e13fa142c526e6252be8ea
Parth Sharma <z@Parth.local>
2025-05-27 15:47:50 +0530

    update

--- trees/SubTreeOfAnotherTree.java
+++ trees/SubTreeOfAnotherTree.java
@@ -5,34 +5,68 @@
 package trees;
 
 public class SubTreeOfAnotherTree {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        String A = serialize(root);
-        String B = serialize(subRoot);
+    // public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    // String A = serialize(root);
+    // String B = serialize(subRoot);
 
-        if (A.contains(B)) {
+    // if (A.contains(B)) {
+    // return true;
+    // }
+
+    // return false;
+    // }
+
+    // public String serialize(TreeNode root) {
+    // StringBuilder str = new StringBuilder();
+    // dfs(root, str);
+
+    // return str.toString();
+    // }
+
+    // private void dfs(TreeNode root, StringBuilder str) {
+    // if (root == null) {
+    // str.append(",#");
+    // return;
+    // }
+
+    // str.append("," + root.val);
+
+    // dfs(root.left, str);
+    // dfs(root.right, str);
+    // }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        boolean l = isSubtree(root.left, subRoot);
+        boolean r = isSubtree(root.right, subRoot);
+
+        if (root.val == subRoot.val) {
+            return isSameTree(root, subRoot) || l || r;
+        }
+
+        return l || r;
+    }
+
+    public boolean isSameTree(TreeNode A, TreeNode B) {
+        if (A == null && B == null) {
             return true;
         }
 
-        return false;
-    }
-
-    public String serialize(TreeNode root) {
-        StringBuilder str = new StringBuilder();
-        dfs(root, str);
-
-        return str.toString();
-    }
-
-    private void dfs(TreeNode root, StringBuilder str) {
-        if (root == null) {
-            str.append(",#");
-            return;
+        if ((A == null && B != null) || (A != null && B == null)) {
+            return false;
         }
 
-        str.append("," + root.val);
+        if (A.val == B.val) {
+            boolean l = isSameTree(A.left, B.left);
+            boolean r = isSameTree(A.right, B.right);
 
-        dfs(root.left, str);
-        dfs(root.right, str);
+            return l && r;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
