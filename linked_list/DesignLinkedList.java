c35959ce1426ac33069124a0d3d3a48a7fdd858d
Parth Sharma <z@Parth.local>
2025-06-08 15:06:46 +0530

    update

---
+++ linked_list/DesignLinkedList.java
@@ -0,0 +1,46 @@
+/*
+ * https://leetcode.com/problems/design-linked-list/description/
+*/
+
+package linked_list;
+
+public class DesignLinkedList {
+
+}
+
+class MyLinkedList {
+
+    public MyLinkedList() {
+
+    }
+
+    public int get(int index) {
+
+    }
+
+    public void addAtHead(int val) {
+
+    }
+
+    public void addAtTail(int val) {
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    public void deleteAtIndex(int index) {
+
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
